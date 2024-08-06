eb5f4136b905535af4228a6da4b10fd96e92a851
Markus Kempe <mk83jaza@studserv.uni-leipzig.de>
2024-08-07 00:37:14 +0200

    Test equals in DiggingPoint

--- src/main/java/DiggingPoint.java
+++ src/main/java/DiggingPoint.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class DiggingPoint {
 	private final int x;
 	private final int y;
@@ -5,5 +7,18 @@ public class DiggingPoint {
 	public DiggingPoint(int x, int y) {
 		this.x = x;
 		this.y = y;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		DiggingPoint that = (DiggingPoint) o;
+		return x == that.x && y == that.y;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(x, y);
 	}
 }
