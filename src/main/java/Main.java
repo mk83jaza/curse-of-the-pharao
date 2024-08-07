cfa80e3ae02608c2da2e89ad0c2c67144b184e0f
Markus Kempe <mk83jaza@studserv.uni-leipzig.de>
2024-08-07 17:07:58 +0200

    Rotate default triangle

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,8 +2,8 @@ import java.util.Random;
 
 public class Main {
 	public static void main(String[] args) {
-		Triangle triangle = new Triangle(new Point(0, 0), new Point(500, 866), new Point(1000, 0));
-		Point startingPoint = new Point(250, 250);
+		Triangle triangle = new Triangle(new Point(0, 866), new Point(500, 0), new Point(1000, 866));
+		Point startingPoint = new Point(0, 0);
 		Model model = new RoyalTriangleModel(triangle, startingPoint);
 		View view = new GUIView(triangle, startingPoint);
 		Controller controller = new Controller(view, model);
